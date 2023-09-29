5a59938ecf7fda4c1f69d8ac3ce9ad40d313321c
Julio Villanueva <75510178+julio-villa@users.noreply.github.com>
2023-09-28 23:39:25 -0500

    fetch data

--- src/components/CourseList.jsx
+++ src/components/CourseList.jsx
@@ -4,7 +4,7 @@ import Course from './Course';
 const CourseList = ({ courses }) => (
     <div className="course-list">
         {Object.values(courses).map(course => (
-            <Course key={course.number} course={course} />
+            <Course key={course.term + course.number} course={course} />
         ))}
     </div>
 );
