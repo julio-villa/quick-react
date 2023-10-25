c83fdff4c0dd8a05bd6a809b4ece4256ab9a2f06
Julio Villanueva <75510178+julio-villa@users.noreply.github.com>
2023-10-25 15:44:02 -0500

    test app launching

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -5,5 +5,10 @@ describe ('Test App', () => {
     it ('launches', () => {
       cy.visit ('/');
     });
+
+    // it ('defaults to fall', () => {
+    //   cy.visit ('/');
+    //   cy.get('[data-cy=course]').should('containe', 'Fall CS')
+    // });
   
   });
